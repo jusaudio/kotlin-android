36e3efaa61c2daf5aa962c6bcb4daf13b2d3794f
Vladimir Peter <vlad.edna@gmail.com>
2019-12-24 14:32:05 +0800

    initial project setup

---
+++ JusAudio/app/src/main/java/com/adrienshen_n_vlad/jus_audio/JusAudioApp.kt
@@ -0,0 +1,6 @@
+package com.adrienshen_n_vlad.jus_audio
+
+import android.app.Application
+
+class JusAudioApp : Application() {
+}
---
+++ JusAudio/app/src/main/java/com/adrienshen_n_vlad/jus_audio/ui/fragments/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.adrienshen_n_vlad.jus_audio.ui.fragments
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
